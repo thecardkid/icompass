669c3f72a8fa5b629f89a38d6176471a6830b23a
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2017-04-29 18:17:30 -0400

    compile all src files into minified files

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		watch: {
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			files: 'css/*.css',
+			tasks: ['cssmin']
+		},
+
+        uglify: {
+			dist: {
+				files: {
+					'public/app.min.js': ['src/js/*.js']
+				}
+			}
+		},
+
+		cssmin: {
+			dist: {
+				files: {
+					'public/app.min.css': ['src/css/*.css']
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', 'watch');
+};
