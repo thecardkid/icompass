b0ab924fcdf2d1495ef5d8b18dfcdc284a871367
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2017-08-20 15:05:20 -0700

    Reorganize imports in icompass
    - Allow configuration of port number

--- icompass.js
+++ icompass.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const path = require('path');
-const port = 8080;
-const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.NODE_PORT || 8080;
 
-var bodyParser = require('body-parser');
-var logger = require('./lib/logger.js');
-var db;
+let app = express();
+let logger = require('./lib/logger.js');
+let db;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -17,12 +17,12 @@ app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-var server = app.listen(port, function() {
+let server = app.listen(port, function() {
     logger.info('Listening on port:', port);
     db = require('./lib/db.js');
 });
 
-var socket = require('./lib/sockets');
+let socket = require('./lib/sockets');
 socket.connect(server);
 
 function cleanup() {
