4b85c1da6f7e78657a6b6db803a9f077d5d81fae
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-03-26 18:45:29 -0400

    Fix mocha tests

--- test/server/Compass.spec.js
+++ test/server/Compass.spec.js
@@ -5,177 +5,185 @@ import { expect } from 'chai';
 import Compass from '../../models/compass';
 import _ from 'underscore';
 
-const CENTER = 'test suite';
+const TOPIC = 'test suite';
 const NOTE = {
-    user: 'mocha',
-    color: '#FFCCCC',
-    text: 'hello, world',
-    style: { bold: false, italic: false, underline: false },
-    doodle: null,
-    isImage: false,
-    x: 0.5,
-    y: 0.5
+  user: 'mocha',
+  color: '#FFCCCC',
+  text: 'hello, world',
+  style: { bold: false, italic: false, underline: false },
+  doodle: null,
+  isImage: false,
+  x: 0.5,
+  y: 0.5,
 };
 
 function clearDB(cb) {
-    for (var i in mongoose.connection.collections)
-        mongoose.connection.collections[i].remove(function() {});
-    cb();
+  for (var i in mongoose.connection.collections)
+    mongoose.connection.collections[i].remove(function() {
+    });
+  cb();
 }
 
 function addNotes(DUT, n, cb) {
-    let i = 0;
-    let incr = (compass) => {
-        i += 1;
-        if (i === n) cb(compass);
-    };
+  let i = 0;
+  let incr = (compass) => {
+    i += 1;
+    if (i === n) cb(compass);
+  };
 
-    for (let j=0; j<n; j++)
-        Compass.addNote(DUT._id, NOTE, incr);
+  for (let j = 0; j < n; j++)
+    Compass.addNote(DUT._id, NOTE, incr);
 }
 
 describe('Compass: models', () => {
-    let DUT;
+  let DUT;
 
-    before(done => {
-        let cb = (err) => {
-            if (err) throw err;
-            done();
-        };
-        if (mongoose.connection.readyState === 0) {
-            if (process.env.NODE_ENV === 'test')
-                mongoose.connect('mongodb://icompass:compass78@ds133311.mlab.com:33311/innovatorscompasshieu', cb);
-            else
-                mongoose.connect('mongodb://localhost/test', cb);
-        }
+  before(done => {
+    let cb = (err) => {
+      if (err) throw err;
+      done();
+    };
+    if (mongoose.connection.readyState === 0) {
+      if (process.env.NODE_ENV === 'test')
+        mongoose.connect('mongodb://icompass:compass78@ds133311.mlab.com:33311/innovatorscompasshieu', cb);
+      else
+        mongoose.connect('mongodb://localhost/test', cb);
+    }
+  });
+
+  after(done => {
+    clearDB(done);
+    mongoose.disconnect();
+  });
+
+  beforeEach(done => {
+    Compass.makeCompass(TOPIC, c => {
+      DUT = c;
+      done();
+    });
+  });
+
+  afterEach(done => {
+    Compass.remove({ _id: DUT._id }, done);
+  });
+
+  it('#makeCompass', (done) => {
+    expect(DUT.editCode).to.have.lengthOf(8);
+    expect(DUT.viewCode).to.have.lengthOf(8);
+    expect(DUT.topic).to.equal(TOPIC);
+    expect(DUT.notes).to.be.empty;
+    done();
+  });
+
+  it('#findByEditCode', (done) => {
+    Compass.findByEditCode(DUT.editCode, (c) => {
+      expect(c.editCode).to.not.be.undefined;
+      expect(c.viewCode).to.not.be.undefined;
+      expect(c.topic).to.equal(TOPIC);
+      done();
+    });
+  });
+
+  it('#findByViewCode', (done) => {
+    Compass.findByViewCode(DUT.viewCode, (c) => {
+      expect(c.editCode).to.be.undefined;
+      expect(c.viewCode).to.not.be.undefined;
+      expect(c.topic).to.equal(TOPIC);
+      done();
+    });
+  });
+
+  it('#setCenter', (done) => {
+    Compass.setCenter(DUT._id, 'center', (c) => {
+      expect(c.center).to.equal('center');
+      done();
+    });
+  });
+
+  it('#findCode', (done) => {
+    let found = false;
+
+    Compass.findCode(DUT.editCode, (c, viewOnly) => {
+      expect(c).to.not.be.null;
+      expect(viewOnly).to.equal(false);
+      if (found) done();
+      else found = true;
     });
 
-    after(done => {
-        clearDB(done);
-        mongoose.disconnect();
+    Compass.findCode(DUT.viewCode, (c, viewOnly) => {
+      expect(c).to.not.be.null;
+      expect(viewOnly).to.equal(true);
+      if (found) done();
+      else found = true;
     });
+  });
 
-    beforeEach(done => {
-        Compass.makeCompass(CENTER, c => {
-            DUT = c;
-            done();
-        });
+  it('#addNote', (done) => {
+    Compass.addNote(DUT._id, NOTE, (c) => {
+      DUT = c;
+      expect(c.notes).to.have.lengthOf(1);
+      done();
     });
+  });
 
-    afterEach(done => {
-        Compass.remove({_id: DUT._id}, done);
-    });
-
-    it('#makeCompass', (done) => {
-        expect(DUT.editCode).to.have.lengthOf(8);
-        expect(DUT.viewCode).to.have.lengthOf(8);
-        expect(DUT.center).to.equal(CENTER);
-        expect(DUT.notes).to.be.empty;
+  it('#updateNote', (done) => {
+    addNotes(DUT, 3, compass => {
+      let updated = Object.assign({}, compass.notes[1]._doc);
+      updated.text = 'Updated';
+      // emulate client side request
+      updated._id = updated._id.toString();
+      Compass.updateNote(compass._id, updated, (c) => {
+        expect(c.notes).to.have.lengthOf(3);
+        expect(c.notes[1].text).to.equal('Updated');
         done();
+      });
     });
+  });
 
-    it('#findByEditCode', (done) => {
-        Compass.findByEditCode(DUT.editCode,(c) => {
-            expect(c.editCode).to.not.be.undefined;
-            expect(c.viewCode).to.not.be.undefined;
-            expect(c.center).to.equal(CENTER);
-            done();
+  it('#bulkUpdateNotes', (done) => {
+    Compass.addNote(DUT._id, NOTE, (c) => {
+      let noteIds = _.map(c.notes, note => note._id.toString());
+      DUT = c;
+
+      let transformation = {
+        style: { bold: true, italic: false, underline: true },
+      };
+      Compass.bulkUpdateNotes(DUT._id, noteIds, transformation, (c) => {
+        _.map(c.notes, (note) => {
+          expect(note.style.bold).to.be.true;
+          expect(note.style.italic).to.be.false;
+          expect(note.style.underline).to.be.true;
         });
+        done();
+      });
     });
+  });
 
-    it('#findByViewCode', (done) => {
-        Compass.findByViewCode(DUT.viewCode, (c) => {
-            expect(c.editCode).to.be.undefined;
-            expect(c.viewCode).to.not.be.undefined;
-            expect(c.center).to.equal(CENTER);
-            done();
-        });
+  it('#deleteNote', (done) => {
+    addNotes(DUT, 2, compass => {
+      Compass.deleteNote(compass._id, compass.notes[1]._id.toString(), (notes, deletedIdx) => {
+        expect(notes).to.have.lengthOf(1);
+        expect(deletedIdx).to.have.lengthOf(1);
+        expect(deletedIdx[0]).to.equal(1);
+        done();
+      });
     });
+  });
 
-    it('#findCode', (done) => {
-        let found = false;
+  it('#deleteNotes', (done) => {
+    addNotes(DUT, 4, compass => {
+      let noteIds = _.map(compass.notes, note => note._id.toString());
+      noteIds.splice(1, 1);
+      expect(noteIds).to.have.lengthOf(3);
 
-        Compass.findCode(DUT.editCode, (c, viewOnly) => {
-            expect(c).to.not.be.null;
-            expect(viewOnly).to.equal(false);
-            if (found) done();
-            else found = true;
-        });
-
-        Compass.findCode(DUT.viewCode, (c, viewOnly) => {
-            expect(c).to.not.be.null;
-            expect(viewOnly).to.equal(true);
-            if (found) done();
-            else found = true;
-        });
+      Compass.deleteNotes(DUT._id, noteIds, (notes, deletedIdx) => {
+        expect(notes).to.have.lengthOf(1);
+        expect(deletedIdx).to.have.lengthOf(3);
+        expect(deletedIdx[0]).to.equal(0);
+        expect(deletedIdx[1]).to.equal(2);
+        expect(deletedIdx[2]).to.equal(3);
+        done();
+      });
     });
-
-    it('#addNote', (done) => {
-        Compass.addNote(DUT._id, NOTE, (c) => {
-            DUT = c;
-            expect(c.notes).to.have.lengthOf(1);
-            done();
-        });
-    });
-
-    it('#updateNote', (done) => {
-        addNotes(DUT, 3, compass => {
-            let updated = Object.assign({}, compass.notes[1]._doc);
-            updated.text = 'Updated';
-            // emulate client side request
-            updated._id = updated._id.toString();
-            Compass.updateNote(compass._id, updated, (c) => {
-                expect(c.notes).to.have.lengthOf(3);
-                expect(c.notes[1].text).to.equal('Updated');
-                done();
-            });
-        });
-    });
-
-    it('#bulkUpdateNotes', (done) => {
-        Compass.addNote(DUT._id, NOTE, (c) => {
-            let noteIds = _.map(c.notes, note => note._id.toString());
-            DUT = c;
-
-            let transformation = {
-                style: { bold: true, italic: false, underline: true }
-            };
-            Compass.bulkUpdateNotes(DUT._id, noteIds, transformation, (c) => {
-                _.map(c.notes, (note) => {
-                    expect(note.style.bold).to.be.true;
-                    expect(note.style.italic).to.be.false;
-                    expect(note.style.underline).to.be.true;
-                });
-                done();
-            });
-        });
-    });
-
-    it('#deleteNote', (done) => {
-        addNotes(DUT, 2, compass => {
-            Compass.deleteNote(compass._id, compass.notes[1]._id.toString(), (notes, deletedIdx) => {
-                expect(notes).to.have.lengthOf(1);
-                expect(deletedIdx).to.have.lengthOf(1);
-                expect(deletedIdx[0]).to.equal(1);
-                done();
-            });
-        });
-    });
-
-    it('#deleteNotes', (done) => {
-        addNotes(DUT, 4, compass => {
-            let noteIds = _.map(compass.notes, note => note._id.toString());
-            noteIds.splice(1, 1);
-            expect(noteIds).to.have.lengthOf(3);
-
-            Compass.deleteNotes(DUT._id, noteIds, (notes, deletedIdx) => {
-                expect(notes).to.have.lengthOf(1);
-                expect(deletedIdx).to.have.lengthOf(3);
-                expect(deletedIdx[0]).to.equal(0);
-                expect(deletedIdx[1]).to.equal(2);
-                expect(deletedIdx[2]).to.equal(3);
-                done();
-            });
-        });
-    });
+  });
 });
