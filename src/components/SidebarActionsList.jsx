d1e79fc230a7c4801a921685899f1600f7c916e7
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-04-10 20:14:38 -0400

    Metrics for sidebar actions list

--- src/components/SidebarActionsList.jsx
+++ src/components/SidebarActionsList.jsx
@@ -7,7 +7,7 @@ import Modal from '../utils/Modal';
 import Socket from '../utils/Socket.js';
 import Storage from '../utils/Storage';
 
-import { MODALS } from '../../lib/constants';
+import { MODALS, HOST } from '../../lib/constants';
 
 export default class SidebarActionsList extends Component {
   constructor(props, context) {
@@ -17,6 +17,7 @@ export default class SidebarActionsList extends Component {
   }
 
   confirmDelete = () => {
+    this.socket.emitMetric('sidebar delete compass');
     this.modal.confirm(MODALS.DELETE_COMPASS, (deleteCompass) => {
       if (deleteCompass) {
         Storage.removeBookmarkByCenter(this.props.compass.center);
@@ -25,24 +26,36 @@ export default class SidebarActionsList extends Component {
     });
   };
 
-  render() {
-    const {
-      togglePrivacyStatement,
-      toggleFeedback,
-    } = this.props.uiX;
+  togglePrivacyStatement = () => {
+    this.socket.emitMetric('sidebar toggle privacy');
+    this.props.uiX.togglePrivacyStatement();
+  };
 
+  toggleFeedback = () => {
+    this.socket.emitMetric('sidebar toggle feedback');
+    this.props.uiX.toggleFeedback();
+  };
+
+  toTutorial = () => {
+    this.socket.emitMetric('sidebar tutorial');
+    window.open(`${HOST}tutorial`, '_blank').focus();
+  };
+
+  render() {
     return (
       <div className="ic-sidebar-list" name="actions">
         <h2>Actions</h2>
-        <button name="privacy" className="ic-action" onClick={togglePrivacyStatement}>
+        <button name="privacy" className="ic-action" onClick={this.togglePrivacyStatement}>
           <i className="material-icons">lock</i>
           <p>privacy</p>
         </button>
-        <button name="tutorial" className="ic-action"><Link to="/tutorial" target="_blank" rel="noopener noreferrer">
-          <i className="material-icons">info</i>
-          <p>tutorial</p>
-        </Link></button>
-        <button name="sucks" className="ic-action" onClick={toggleFeedback}>
+        <button name="tutorial" className="ic-action">
+          <Link onClick={this.toTutorial} to="/tutorial" target="_blank" rel="noopener noreferrer">
+            <i className="material-icons">info</i>
+            <p>tutorial</p>
+          </Link>
+        </button>
+        <button name="sucks" className="ic-action" onClick={this.toggleFeedback}>
           <i className="material-icons">chat_bubble</i>
           <p>feedback</p>
         </button>
