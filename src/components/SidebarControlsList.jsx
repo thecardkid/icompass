6dcc16693baa9ce3374fc93161dcc5664e008394
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-04-10 20:06:51 -0400

    Metrics for sidebar controls list

--- src/components/SidebarControlsList.jsx
+++ src/components/SidebarControlsList.jsx
@@ -8,27 +8,52 @@ export default class SidebarControlsList extends Component {
     this.socket = new Socket();
   }
 
+  showNewNote = () => {
+    this.socket.emitMetric('sidebar new note');
+    this.props.uiX.showNewNote();
+  };
+
+  showDoodle = () => {
+    this.socket.emitMetric('sidebar new doodle');
+    this.props.uiX.showDoodle();
+  };
+
+  toggleSidebar = () => {
+    this.socket.emitMetric('sidebar toggle sidebar');
+    this.props.uiX.toggleSidebar();
+  };
+
+  toggleChat = () => {
+    this.socket.emitMetric('sidebar toggle chat');
+    this.props.uiX.toggleChat();
+  };
+
+  toggleAbout = () => {
+    this.socket.emitMetric('sidebar toggle prompt');
+    this.props.uiX.toggleAbout();
+  };
+
   render() {
     return (
       <div className="ic-sidebar-list" name="controls">
         <h2>Controls</h2>
-        <button className="ic-action" onClick={() => this.props.uiX.showNewNote()}>
+        <button className="ic-action" onClick={this.showNewNote}>
           <span className='ic-ctrl-key'>n</span>
           <p>new note</p>
         </button>
-        <button className="ic-action" onClick={this.props.uiX.showDoodle}>
+        <button className="ic-action" onClick={this.showDoodle}>
           <span className='ic-ctrl-key'>d</span>
           <p>new doodle</p>
         </button>
-        <button className="ic-action" onClick={this.props.uiX.toggleSidebar}>
+        <button className="ic-action" onClick={this.toggleSidebar}>
           <span className='ic-ctrl-key'>s</span>
           <p>toggle sidebar</p>
         </button>
-        <button className="ic-action" onClick={this.props.uiX.toggleChat}>
+        <button className="ic-action" onClick={this.toggleChat}>
           <span className='ic-ctrl-key'>c</span>
           <p>toggle chat</p>
         </button>
-        <button className="ic-action" onClick={this.props.uiX.toggleAbout}>
+        <button className="ic-action" onClick={this.toggleAbout}>
           <span className='ic-ctrl-key'>p</span>
           <p>toggle prompt</p>
         </button>
