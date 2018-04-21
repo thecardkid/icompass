3e4938d99decfb29ec84cea3ae42732e0bb67190
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2018-04-21 10:38:39 -0400

    Render submenus of workspace menu

--- src/components/WorkspaceMenu.js
+++ src/components/WorkspaceMenu.js
@@ -8,15 +8,89 @@ export default class WorkspaceMenu extends Component {
 
     this.state = {
       active: false,
+      submenus: {
+        share: false,
+        notes: false,
+        modes: false,
+      },
     };
     this.socket = SocketSingleton.getInstance();
     this.toast = ToastSingleton.getInstance();
   }
 
+  showSubmenu = (submenu) => () => {
+    this.setState({ submenus: {
+      share: false,
+      notes: false,
+      modes: false,
+      [submenu]: true,
+    }});
+  };
+
+  renderShareSubmenu = () => {
+    return (
+      <div className={'ic-menu ic-share-submenu'}>
+        <section>
+          <div className={'ic-menu-item'}>
+            Edit Link
+          </div>
+          <div className={'ic-menu-item'}>
+            View-Only Link
+          </div>
+          <div className={'ic-menu-item'}>
+            Export to PDF
+          </div>
+          <div className={'ic-menu-item'}>
+            Twitter
+          </div>
+        </section>
+      </div>
+    );
+  };
+
+  renderNotesSubmenu = () => {
+    return (
+      <div className={'ic-menu ic-notes-submenu'}>
+        <section>
+          <div className={'ic-menu-item'}>
+            Text Note
+          </div>
+          <div className={'ic-menu-item'}>
+            Doodle Note
+          </div>
+        </section>
+      </div>
+    );
+  };
+
+  renderModesSubmenu = () => {
+    return (
+      <div className={'ic-menu ic-modes-submenu'}>
+        <section>
+          <div className={'ic-menu-item'}>
+            Standard
+          </div>
+          <div className={'ic-menu-item'}>
+            Compact
+          </div>
+          <div className={'ic-menu-item'}>
+            Draft
+          </div>
+          <div className={'ic-menu-item'}>
+            Bulk Edit
+          </div>
+        </section>
+      </div>
+    );
+  };
+
   renderMenu = () => {
     return (
       <div className={'ic-menu ic-workspace-menu'}>
         <section className={'border-bottom'}>
+          <div className={'ic-menu-item'}>
+            New Workspace
+          </div>
           <div className={'ic-menu-item'}>
             Email Reminder
           </div>
@@ -25,22 +99,22 @@ export default class WorkspaceMenu extends Component {
           </div>
         </section>
         <section className={'border-bottom'}>
-          <div className={'ic-menu-item'}>
-            New Workspace
-          </div>
-          <div className={'ic-menu-item'}>
+          <div className={'ic-menu-item has-more'} onMouseOver={this.showSubmenu('share')}>
+            {this.state.submenus.share && this.renderShareSubmenu()}
             Share
           </div>
-          <div className={'ic-menu-item'}>
+          <div className={'ic-menu-item has-more'} onMouseOver={this.showSubmenu('notes')}>
             Notes
+            {this.state.submenus.notes && this.renderNotesSubmenu()}
           </div>
-          <div className={'ic-menu-item'}>
+          <div className={'ic-menu-item has-more'} onMouseOver={this.showSubmenu('modes')}>
             Editing Modes
+            {this.state.submenus.modes && this.renderModesSubmenu()}
           </div>
         </section>
         <section>
           <div className={'ic-menu-item'}>
-            Log out
+            Log Out
           </div>
           <div className={'ic-menu-item'}>
             Delete Workspace
