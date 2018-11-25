d687e552f7adae0ada51f9a40864dd1afb0506b0
Hieu Nguyen <hieu@shift.com>
2018-11-25 10:28:59 -0800

    Add tooltip and header to toolbar

--- src/components/BulkEditToolbar.jsx
+++ src/components/BulkEditToolbar.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 import { connect } from 'react-redux';
+import ReactTooltip from 'react-tooltip';
 import { bindActionCreators } from 'redux';
 import _ from 'underscore';
 
 import Modal from '../utils/Modal';
+import Storage from '../utils/Storage';
 
 import * as uiX from '../actions/ui';
 import * as workspaceX from '../actions/workspace';
@@ -60,17 +62,37 @@ class BulkEditToolbar extends Component {
   render() {
     if (!this.props.show) return null;
 
+    const tooltipType = Storage.getDarkTheme() ? 'light' : 'dark';
+
     return (
       <Draggable>
         <div id="ic-visual-toolbar">
-          <div>
-            <button id="ic-bulk-submit" onClick={this.submit}>
-              <i className={'material-icons'}>check</i>
-            </button>
-            <FormPalette setColor={this.bulkColor} color={this.props.workspace.color}/>
-            <button id="ic-bulk-delete" onClick={this.bulkDelete}>
+          <div id={'header'}>
+            Bulk Edit Toolbar
+          </div>
+          <hr />
+          <div id={'actions'}>
+            <button id="ic-bulk-delete"
+                    onClick={this.bulkDelete}
+                    data-tip="Delete notes"
+                    data-for="bulk-delete-tooltip">
               <i className={'material-icons'}>delete</i>
             </button>
+            <ReactTooltip id={'bulk-delete-tooltip'} place={'bottom'} effect={'solid'} type={tooltipType} delayShow={100}/>
+
+            <div data-tip="Color notes"
+                 data-for="bulk-color-tooltip">
+              <FormPalette setColor={this.bulkColor} color={this.props.workspace.color}/>
+            </div>
+            <ReactTooltip id={'bulk-color-tooltip'} place={'bottom'} effect={'solid'} type={tooltipType} delayShow={100}/>
+
+            <button id="ic-bulk-submit"
+                    onClick={this.submit}
+                    data-tip="Save"
+                    data-for="bulk-commit">
+              <i className={'material-icons'}>check</i>
+            </button>
+            <ReactTooltip id={'bulk-commit'} place={'bottom'} effect={'solid'} type={tooltipType} delayShow={100}/>
           </div>
         </div>
       </Draggable>
