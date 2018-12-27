49a75d1d456fff98de6ad1d71d6b9d8c8f181e9a
Hieu Nguyen <hieu@shift.com>
2018-12-27 12:27:19 -0800

    Merge branch "scripts"
    reset
    
    Add run script
    
    Add merge script
    
    Add +x
    
    Modify merge script
    
    Modify
    
    Test
    
    change
    
    Remove quotes
    
    merge
    
    Finish merge
    
    Change branch to develop

---
+++ scripts/merge.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curr_branch="$(git branch | grep \* | cut -d ' ' -f2)"
+if [[ "$curr_branch" == "develop" ]]; then
+    echo "Need to be on feature branch to merge" >&2
+    exit 1
+fi
+
+commit_messages="$(git log --format=%B --reverse develop..${curr_branch})"
+
+cat <<EOF >&2
+
+
+about to squash merge branch "$curr_branch" into develop with the following commit message:
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Merge branch "$curr_branch"
+
+$commit_messages
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+EOF
+
+read -p "Continue? [y/N] " input >&2
+if [[ $input != "y" && $input != "Y" ]]; then
+    echo "aborting" >&2
+    exit 1
+fi
+
+# check branch is not dirty
+status="$(git status -s)"
+if [[ ! -z "$status" ]]; then
+    echo "aborting, git branch is dirty" >&2
+    exit 1
+fi
+
+echo "committing..." >&2
+git checkout develop
+git merge --squash "$curr_branch"
+git commit -m "Merge branch \"$curr_branch\"" -m "$commit_messages"
+
+git status
